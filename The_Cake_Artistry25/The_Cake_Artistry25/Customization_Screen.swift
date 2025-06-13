64c596c9eb2c72b6c01e0a078a518a1563f0b632
Yatin005 <68661297+Yatin005@users.noreply.github.com>
2025-06-13 15:17:01 -0400

    commit

---
+++ The_Cake_Artistry25/The_Cake_Artistry25/Customization_Screen.swift
@@ -0,0 +1,18 @@
+//
+//  Customization_Screen.swift
+//  The_Cake_Artistry25
+//
+//  Created by Yatin Parulkar on 2025-06-13.
+//
+
+import SwiftUI
+
+struct Customization_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Customization_Screen()
+}
---
+++ The_Cake_Artistry25/The_Cake_Artistry25/Gallery_Screen.swift
@@ -0,0 +1,18 @@
+//
+//  Gallery_Screen.swift
+//  The_Cake_Artistry25
+//
+//  Created by Yatin Parulkar on 2025-06-13.
+//
+
+import SwiftUI
+
+struct Gallery_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gallery_Screen()
+}
